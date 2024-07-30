66ccb111d01ab86f06f1814704be3a01836d8af0
TheAyushThakur <ayushthakur6005@gmail.com>
2024-07-30 19:07:26 +0530

    commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-
-export const dbConnection = ()=>{
-    mongoose.connect(process.env.MONGO_URI, {
-        dbName: "MERN_STACK_EHC_DEPLOYED"
-    }).then(()=>{
-        console.log("connected to database!");
-    }).catch(err=>{
-        console.log(`some error occured while connected to database: ${err}`);
-    });
-
-
+export const dbConnection = () => {
+  mongoose.connect(process.env.MONGO_URI, {
+    dbName: "MERN_STACK_EHC_DEPLOYED",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase server selection timeout to 30 seconds
+    socketTimeoutMS: 45000, // Increase socket timeout to 45 seconds
+  })
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(err => {
+    console.log(`Some error occurred while connecting to database: ${err}`);
+  });
 };

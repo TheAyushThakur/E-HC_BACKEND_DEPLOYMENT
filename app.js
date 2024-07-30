b9925d4f8c51693cd483fd394913916525d0a071
TheAyushThakur <ayushthakur6005@gmail.com>
2024-07-30 20:32:45 +0530

    commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import appointmentRouter from "./router/appointmentRouter.js";
 const app = express();
 config({ path: "./config/config.env" });
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));

a928a659ecdd0401f869d72a3c732c7bde7e09d6
TheAyushThakur <ayushthakur6005@gmail.com>
2024-07-30 19:15:00 +0530

    commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose.connect(process.env.MONGO_URI, {
-    dbName: "MERN_STACK_EHC_DEPLOYED",
+    dbName: "EHC_DEPLOYED",
   })
   .then(() => {
     console.log("Connected to database!");
